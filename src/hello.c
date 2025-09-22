de09064e7bd2a5f1fa6ecb67a0f2a769e66fae40
DELWIN BIJU <delwinbijuap@gmail.com>
2025-09-22 08:50:09 +0000

    adjust formatting for hello world

--- src/hello.c
+++ src/hello.c
@@ -1,8 +1,8 @@
 
 #include <stdio.h>
-
+//this is your first c program my friend
 int main(int argc, char *argv[]) {
-  // This is your first C program my friend
+  // This is your first C program my friend lab in 2025
   printf("Hello, RTU World from C Lab in 2025!\n");
   printf("You passed %d argument(s).\n", argc - 1);
   for (int i = 1; i < argc; ++i) {
